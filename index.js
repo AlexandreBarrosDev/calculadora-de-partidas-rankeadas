ba8e29ce24f3ee7c246a5dbb116c508aa16efcfb
Alexandre Barros <alexandrebarros.dev@gmail.com>
2024-10-08 09:57:10 -0300

    Desafio atualizadocom inclusão do index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+let rankedResults = rankedCalculator(56, 22);
+let rankedLevel = "Ferro"
+
+function rankedCalculator(rankedVictories, rankedDefeats) {
+
+    return rankedVictories - rankedDefeats;
+}
+
+switch(true) {
+    case (rankedResults <= 10):
+        rankedLevel = "Ferro";
+        break;
+    case (rankedResults > 11 && rankedResults <= 20):
+        rankedLevel = "Bronze";
+        break;
+    case (rankedResults > 21 && rankedResults <= 50):
+        rankedLevel = "Prata";
+        break;
+    case (rankedResults >= 51 && rankedResults <= 80):
+        rankedLevel = "Ouro";
+        break;
+    case (rankedResults >= 81 && rankedResults <= 90):
+        rankedLevel = "Diamante";
+        break;
+    case (rankedResults >= 91 && rankedResults <= 100):
+        rankedLevel = "Lendário";
+        break;
+    default:
+        rankedLevel = "Imortal";
+        break;
+}
+
+console.log(`O Herói tem saldo de ${rankedResults} está no nível de ${rankedLevel}.`);
+
